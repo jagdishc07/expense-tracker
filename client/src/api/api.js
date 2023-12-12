d87b4e03a204a4fc5ea98306976cf76b9bc97011
Jagdish Choudhary <jagdishchoudhary8570@gmail.com>
2023-12-12 21:02:45 +0530

    vercel.config

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const host = "http://localhost:5000";
+// const host = "http://localhost:5000";
+const host = "https://expense-tracker-jagdish0809.vercel.app/";
 
 export const newExpense = async (data) => {
     try {
